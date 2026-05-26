f5aa08c0177d79fe8e1ab6079da9c94dd4bc0e54
tolonjan <jjtolonen@gmail.com>
2026-05-26 14:42:24 +0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{padding:"40px"}}>
+      <h1>TarjousAI</h1>
+      <p>Voita enemmän kilpailutuksia tekoälyn avulla.</p>
+    </main>
+  )
+}
